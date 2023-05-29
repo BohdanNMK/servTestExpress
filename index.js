59e19c1bca74ea5c833000da57add3dd18a837b3
Bohdan Motenko <117380980+BohdanNMK@users.noreply.github.com>
2023-05-30 02:42:40 +0300

    Виправлено 1.У всіх методах зроблено чітке призначення Number та String згідно ТЗ. 2.У Get методі виправив корректність роботи query запитів та додано максимальну довжину змінної name

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
 const express = require('express');
-const { ID: id, PORT, ID } = require('./veribals');
+const { ID, PORT } = require('./veribals');
 const app = express();
 
 
 app.use(express.json());
-      app.post('/v1/test', (req, res) => {
-        const { name, type } = req.body;
+app.post('/v1/test', (req, res) => {
+  const { name, type } = req.body;
 
-        if (!name || !type) {
-          return res.status(400).json({ error: `ім'я та тип є обов'язковими полями` });
-        }
-        if (name.length > 255) {
-          return res.status(400).json({ error: `Ім'я має містити максимум 255 символів` });
-        }
-        res.status(201).json({ ID: id });
-      });
+  if (!name || !type) {
+    return res.status(400).json({ error: `ім'я та тип є обов'язковими полями` });
+  }
+  if (typeof name !== 'string' || name.length > 255) {
+    return res.status(400).json({ error: `Поле "name" має бути рядком і містити максимум 255 символів` });
+  }
+  if (typeof type !== 'number') {
+    return res.status(400).json({ error: `Поле "type" має бути числом` });
+  }
+  res.status(201).json({ ID });
+});
 
 
-              app.put('/v1/test/:id', (req, res) => {
-                const { id } = req.params;
-                const type = req.body.type;
+app.put('/v1/test/:id', (req, res) => {
+  const { id } = req.params;
+  const { type } = req.body;
 
-                if (Number(id) === ID) {
-                  if (type !== undefined) {
-                    const response = { ID: id, type };
-                    return res.status(200).json(response);
-                  } else {
-                    return res.status(400).json({ error: 'Поле "type" є обов\'язковим.' });
-                  }
-                } else {
-                  return res.status(404).json({ error: 'Запит не знайдено.' });
-                }
-              });
+  if (Number(id) === ID) {
+    if (type !== undefined) {
+      const response = { id: Number(id), type: Number(type) };
+      return res.status(200).json(response);
+    } else {
+      return res.status(204).send();
+    }
+  } else {
+    return res.status(404).json({ error: 'Запит не знайдено.' });
+  }
+});
 
-      app.get('/v1/test/:name/:type', (req, res) => {
-        const { name, type } = req.params;
 
-        if (name === 'pest' && Number(type) === 123) {
-          return res.status(200).json({ ID: id, name: 'pest', type: 123 });
-        } else {
-          return res.status(404).json({ error: 'Запит не знайдено.' });
-        }
-      });
+app.get('/v1/test', (req, res) => {
+  const { name, type } = req.query;
+
+  if (name === 'pest' && Number(type) === 123) {
+    return res.status(200).json({ ID, name: 'pest', type: 123 });
+  } else if (name.length > 255) {
+    return res.status(400).json({ error: `Ім'я має містити максимум 255 символів` });
+  } else {
+    return res.status(404).json({ error: 'Запит не знайдено.' });
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`Сервер працює на порту ${PORT}`);
