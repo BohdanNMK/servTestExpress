eb953ba23dae3dacf347bf73c60a598b4c337a1a
Bohdan Motenko <117380980+BohdanNMK@users.noreply.github.com>
2023-06-02 16:51:24 +0300

    Changed language in description, Added error handling function. corrected response statuses

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const express = require('express');
-const { ID, PORT } = require('./veribals');
+const { ID, PORT } = require('./variable');
 const app = express();
 
 
 app.use(express.json());
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send('Bad Request');
+});
+
+
 app.post('/v1/test', (req, res) => {
   const { name, type } = req.body;
 
   if (!name || !type) {
-    return res.status(400).json({ error: `ім'я та тип є обов'язковими полями` });
+    return res.status(400).json({ error: `'name' and 'type' are required fields` });
   }
   if (typeof name !== 'string' || name.length > 255) {
-    return res.status(400).json({ error: `Поле "name" має бути рядком і містити максимум 255 символів` });
+    return res.status(400).json({ error: `"name" field must be a string with a maximum of 255 characters` });
   }
   if (typeof type !== 'number') {
-    return res.status(400).json({ error: `Поле "type" має бути числом` });
+    return res.status(400).json({ error: `Field "type" must be a number` });
   }
-  res.status(201).json({ ID });
+  res.status(201).json({ id:ID });
 });
 
 
@@ -24,33 +30,33 @@ app.put('/v1/test/:id', (req, res) => {
   const { id } = req.params;
   const { type } = req.body;
 
-  if (Number(id) === ID) {
-    if (type !== undefined) {
-      const response = { id: Number(id), type: Number(type) };
-      return res.status(200).json(response);
-    } else {
-      return res.status(204).send();
-    }
+  if (!/^[a-zA-Z0-9]+$/.test(id) || isNaN(id) || Number(id) !== ID) {
+    return res.status(400).json({ error: 'Bad Request' });
+  }
+  if (type !== undefined) {
+    const response = { id: Number(id), type: Number(type) };
+    return res.status(200).json(response);
   } else {
-    return res.status(404).json({ error: 'Запит не знайдено.' });
+    return res.status(204).send();
   }
 });
 
 
 app.get('/v1/test', (req, res) => {
-  const { name, type } = req.query;
+  const { name, type } = req.query
 
-  if ( name === 'pest' && Number(type) === 123) {
-    return res.status(200).json({ ID, name: 'pest', type: 123 });
-  } else if (typeof name !== 'string' || name.length > 255) {
-    return res.status(400).json({ error: `Ім'я має містити максимум 255 символів` });
+  if (!name && !type) {
+    return res.status(422).json({ error: 'Unprocessable Entity' });
+  } else if (name === 'pest' && Number(type) === 123) {
+    return res.status(200).json({ id: ID, name: 'pest', type: 123 });
+  } else if (typeof name !== 'string' || name.length > 255 || !name) {
+    return res.status(400).json({ error: `Bad Request` });
   } else {
-    return res.status(404).json({ error: 'Запит не знайдено.' });
+    return res.status(404).json({ error: 'Not Found' });
   }
 });
 
 
 app.listen(PORT, () => {
-  console.log(`Сервер працює на порту ${PORT}`);
+  console.log(`server runs on the port ${PORT}`);
 });
-
