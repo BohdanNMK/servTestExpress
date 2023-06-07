3d5104ed663d10fd3580e079b75a2318d4b12d1e
Bohdan Motenko <117380980+BohdanNMK@users.noreply.github.com>
2023-06-07 14:14:03 +0300

    modified check in GET method. Posted variables and statuses to a separate file.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { ID, PORT } = require('./variable');
+const { ID, PORT,MAX_LENGTH,TYPE_VALUE,HTTP_STATUS,HTTP_MESSAGES } = require('./variable');
 const app = express();
 
 
 app.use(express.json());
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(400).send('Bad Request');
+  res.status(400).json({error: HTTP_MESSAGES[HTTP_STATUS.BAD_REQUEST] });//чи можна таким чином(чи дозволений такий формат)?
 });
 
 
@@ -14,16 +14,15 @@ app.post('/v1/test', (req, res) => {
   const { name, type } = req.body;
 
   if (!name || !type) {
-    return res.status(400).json({ error: `'name' and 'type' are required fields` });
+    return res.status(400).json({ error: HTTP_MESSAGES[400.1] });//Чи допустимий таким чином формат помилки?
   }
-  if (typeof name !== 'string' || name.length > 255) {
-    return res.status(400).json({ error: `"name" field must be a string with a maximum of 255 characters` });
+  if (typeof name !== 'string' || name.length > MAX_LENGTH) {
+    return res.status(400).json({ error: HTTP_MESSAGES[400.2] });
   }
   if (typeof type !== 'number') {
-    return res.status(400).json({ error: `Field "type" must be a number` });
+    return res.status(400).json({ error: HTTP_MESSAGES[400.3] });
   }
   res.status(201).json({ id:ID });
-  app.use(err,next)
 });
 
 
@@ -31,14 +30,14 @@ app.put('/v1/test/:id', (req, res) => {
   const { id } = req.params;
   const { type } = req.body;
 
-  if (!/^[0-9]+$/.test(id) || isNaN(id) !== ID) {
-    return res.status(400).json({ error: 'Bad Request' });
+  if (!/^[0-9]+$/.test(id) || parseInt(id) !== ID) {
+    return res.status(400).json({ error: HTTP_MESSAGES[400] });
   }
   if (type !== undefined) {
-    const response = { id: Number(id), type: Number(type) };
+    const response = { id: parseInt(id), type: parseInt(type) };
     return res.status(200).json(response);
   } else {
-    return res.status(204).send();
+    return res.status(204).json();
   }
 });
 
@@ -46,21 +45,22 @@ app.put('/v1/test/:id', (req, res) => {
 app.get('/v1/test', (req, res) => {
   const { name, type } = req.query;
 
-  if (name === 'pest' && parseInt(type) === 123) {
-    const response = { id: ID, name: 'pest', type: 123 };
+  if (name === 'pest' && typeof type === 'string' && parseInt(type) === TYPE_VALUE) {
+    const response = { id: ID, name: 'pest', type: TYPE_VALUE };
     return res.status(200).json(response);
   } else if (!name && !type) {
-    return res.status(204).send();
+    return res.status(204).json();
   } else if (
     (name && typeof name !== 'string') ||
-    (name && name.length > 255) ||
-    (type && (typeof type !== 'number' || isNaN(type)))
+    (name && name.length > MAX_LENGTH) ||
+    (type && (typeof type !== 'string' || isNaN(parseInt(type)) || parseInt(type) !== TYPE_VALUE))
   ) {
-    return res.status(400).json({ error: 'Bad Request' });
+    return res.status(400).json({ error: HTTP_MESSAGES[400] });
   } else {
-    return res.status(404).json({ error: 'Not Found' });
+    return res.status(404).json({ error: HTTP_MESSAGES[404] });//??? не зовсім зрозучів чому не 404.
   }
 });
+
 
 
 app.listen(PORT, () => {
--- variable.js
+++ variable.js
@@ -1,5 +1,27 @@
 module.exports = {
     ID:111,
     PORT:33111,
+    MAX_LENGTH:255,
+    TYPE_VALUE:123,
+    HTTP_STATUS: {
+        OK: 200,
+        CREATED: 201,
+        NO_CONTENT: 204,
+        BAD_REQUEST: 400,
+        NOT_FOUND: 404,
+        UNPROCESSABLE_ENTITY: 422,
+      },
+      HTTP_MESSAGES: {
+        200: "OK",
+        201: "Created",
+        204: "No Content",
+        400: "Bad Request",
+        400.1: "'name' and 'type' are required fields",
+        400.2: "'name' field must be a string with a maximum of 255 characters",
+        400.3: "Field 'type' must be a number",
+        404: "Not Found",
+        422: "Unprocessable Entity",
+      },
+    };
+
     
-};
